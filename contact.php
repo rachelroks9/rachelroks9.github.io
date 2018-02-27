b5ea87e604f67d0eead0b3fb18d7f211954de6ef
rachelroks9 <rachelroks9@yahoo.com>
2018-02-26 23:33:59 -0500

    New contact php?

--- contact.php
+++ contact.php
@@ -4,10 +4,10 @@ require('recaptcha-master/src/autoload.php');
 
 // configure
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'rachelbethblacker@gmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = 'rachelbethblacker@gmail.com';
 
 // subject of the email
 $subject = 'New message from contact form';
